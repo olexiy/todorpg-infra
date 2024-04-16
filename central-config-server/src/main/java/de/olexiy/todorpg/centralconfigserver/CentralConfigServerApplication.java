7318c57efab887539fb84ad367c87cb35a0ae5cd
Olexiy Sokurenko <sokurenko-extern@gvl.de>
2024-04-16 21:28:14 +0200

    Initial commit

---
+++ central-config-server/src/main/java/de/olexiy/todorpg/centralconfigserver/CentralConfigServerApplication.java
@@ -0,0 +1,15 @@
+package de.olexiy.todorpg.centralconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CentralConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralConfigServerApplication.class, args);
+	}
+
+}
---
+++ central-config-server/src/test/java/de/olexiy/todorpg/centralconfigserver/CentralConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.olexiy.todorpg.centralconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
